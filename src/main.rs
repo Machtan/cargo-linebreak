db1b5b772a1862e76c3c6e2927d60a963410bfae
Machtan <jako3047@gmail.com>
2015-12-17 14:24:32 +0100

    Allow the first argument to be 'linebreak' and improve the title on help/usage messages

--- src/main.rs
+++ src/main.rs
@@ -33,14 +33,26 @@ fn main() {
     let mut prefix = "\n".to_string();
     let mut suffix = "\n".to_string();
     let mut n: usize = 80;
+    let mut run_from_cargo = "".to_string();
     {
         let mut parser = ArgumentParser::new();
+        let mut title = "cargo-linebreak";
+        
+        let args: Vec<String> = std::env::args().collect();
+        if args.len() > 1 && args[1] == "linebreak" {
+            title = "cargo linebreak"
+        }
+        
+        parser.set_title(title);
         
         parser.set_description("
         Prints a line of characters based on input.
         Intended to be used with the cargo-watch tool,
         eg: 'cargo watch linebreak check'
         ");
+        
+        parser.refer(&mut run_from_cargo).add_argument("linebreak", Store,
+            "Workaround for cargo passing the name of the subcommand");
         
         parser.refer(&mut ch).add_option(&["-c", "--char"], Store, 
             "The character to fill the line with. Default '='");
@@ -55,6 +67,8 @@ fn main() {
             "The text to print after the line. Default \"\\n\"");
         
         parser.parse_args_or_exit();
+        
+        
     }
     let line: String = (0..n).map(|_| {let Char(c) = ch; c}).collect();
     println!("{}{}{}", prefix, line, suffix);
